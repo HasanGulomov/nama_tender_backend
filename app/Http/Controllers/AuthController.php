54d1893ba95d23cbe326079d669f1c175c7f7777
HasanGulomov <gulomovhasan194@gmail.com>
2026-03-25 11:50:05 +0500

    Logindan username olib tashland

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,12 +8,12 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller 
 {
-    // REGISTER - Username, Email va Password bilan ro'yxatdan o'tish
+    
     public function register(Request $request)
     {
         $request->validate([
-            'username' => 'required|string|unique:users,username', // Username band bo'lmasligi kerak
-            'email'    => 'required|string|email|unique:users,email', // Email band bo'lmasligi kerak
+            'username' => 'required|string|unique:users,username', 
+            'email'    => 'required|string|email|unique:users,email',
             'password' => 'required|string|min:6',
         ]);
 
@@ -30,23 +30,20 @@ class AuthController extends Controller
         ], 201);
     }
 
-    // LOGIN - Username, Email va Password hammasi mos kelishi shart
+    
     public function login(Request $request)
     {
         $request->validate([
-            'username' => 'required|string',
             'email'    => 'required|string|email',
             'password' => 'required|string',
         ]);
 
-        // Bazadan username va email bo'yicha qidiramiz
-        $user = User::where('username', $request->username)
-                    ->where('email', $request->email)
-                    ->first();
+        
+        $user = User::where('email', $request->email)->first();
 
-        // Tekshiruv: Foydalanuvchi bormi va parol to'g'rimi?
+      
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'Username, email yoki parol xato!'], 401);
+            return response()->json(['message' => 'Email, yoki parol xato!'], 401);
         }
 
         return response()->json([
