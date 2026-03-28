39cda7386656c4d6ce1e71788d27e9549936c3ba
HasanGulomov <gulomovhasan194@gmail.com>
2026-03-28 09:19:14 +0500

    tayor

--- app/Http/Controllers/TenderController.php
+++ app/Http/Controllers/TenderController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Tender;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TenderController extends Controller
 {
-    
     public function index(Request $request)
     {
         $query = Tender::query();
 
-        
         $query->when($request->category, fn($q) => 
             $q->where('category', $request->category)
         )
@@ -32,13 +31,35 @@ class TenderController extends Controller
             $q->where('budget', '<=', $request->max_budget)
         );
 
-        
         $tenders = $query->latest()->paginate(10)->appends($request->query());
 
         return response()->json($tenders);
     }
 
-   
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'category' => 'required|string',
+            'location' => 'required|string',
+            'deadline' => 'required|date',
+            'budget' => 'required|numeric',
+            'source'      => 'required|string'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $tender = Tender::create($request->all());
+
+        return response()->json([
+            'message' => 'Tender muvaffaqiyatli yaratildi',
+            'data' => $tender
+        ], 201);
+    }
+
     public function show($id)
     {
         $tender = Tender::find($id);
@@ -50,8 +71,46 @@ class TenderController extends Controller
         return response()->json($tender);
     }
 
-    
-   
+    public function update(Request $request, $id)
+    {
+        $tender = Tender::find($id);
+
+        if (!$tender) {
+            return response()->json(['message' => 'Tender topilmadi'], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string',
+            'category' => 'sometimes|string',
+            'location' => 'sometimes|string',
+            'deadline' => 'sometimes|date',
+            'budget' => 'sometimes|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $tender->update($request->all());
+
+        return response()->json([
+            'message' => 'Tender yangilandi',
+            'data' => $tender
+        ]);
+    }
+    public function destroy($id)
+    {
+        $tender = Tender::find($id);
+
+        if (!$tender) {
+            return response()->json(['message' => 'Tender topilmadi'], 404);
+        }
+
+        $tender->delete();
+
+        return response()->json(['message' => 'Tender muvaffaqiyatli o‘chirildi']);
+    }
     public function toggleFavorite(Request $request, $id)
     {
         $user = $request->user();
@@ -61,8 +120,6 @@ class TenderController extends Controller
         }
 
         $tender = Tender::findOrFail($id);
-        
-      
         $user->favorites()->toggle($tender->id);
 
         return response()->json([   
@@ -80,6 +137,7 @@ class TenderController extends Controller
             return response()->json(['message' => 'Avtorizatsiyadan oting'], 401);
         }
 
+      
         return response()->json($user->favorites);
     }
 }
